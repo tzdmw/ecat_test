8d7d11209e0b16645162a90b353c58791bcd2e06
tzdmw <3077147606@qq.com>
2023-11-27 20:07:23 +0800

    init fw

---
+++ ecat_test/cmake-build-debug/CMakeFiles/ecat_test.elf.dir/DependInfo.cmake
@@ -0,0 +1,65 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "D:/fw/motor_ws/ecat/ecat_test/Core/Startup/startup_stm32f405rgtx.s" "D:/fw/motor_ws/ecat/ecat_test/cmake-build-debug/CMakeFiles/ecat_test.elf.dir/Core/Startup/startup_stm32f405rgtx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F405xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "D:/fw/motor_ws/ecat/ecat_test/Core/Inc"
+  "D:/fw/motor_ws/ecat/ecat_test/Drivers/STM32F4xx_HAL_Driver/Inc"
+  "D:/fw/motor_ws/ecat/ecat_test/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "D:/fw/motor_ws/ecat/ecat_test/Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "D:/fw/motor_ws/ecat/ecat_test/Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/fw/motor_ws/ecat/ecat_test/Core/Src/gpio.c" "CMakeFiles/ecat_test.elf.dir/Core/Src/gpio.c.obj" "gcc" "CMakeFiles/ecat_test.elf.dir/Core/Src/gpio.c.obj.d"
+  "D:/fw/motor_ws/ecat/ecat_test/Core/Src/main.c" "CMakeFiles/ecat_test.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/ecat_test.elf.dir/Core/Src/main.c.obj.d"
+  "D:/fw/motor_ws/ecat/ecat_test/Core/Src/spi.c" "CMakeFiles/ecat_test.elf.dir/Core/Src/spi.c.obj" "gcc" "CMakeFiles/ecat_test.elf.dir/Core/Src/spi.c.obj.d"
+  "D:/fw/motor_ws/ecat/ecat_test/Core/Src/stm32f4xx_hal_msp.c" "CMakeFiles/ecat_test.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj" "gcc" "CMakeFiles/ecat_test.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj.d"
+  "D:/fw/motor_ws/ecat/ecat_test/Core/Src/stm32f4xx_it.c" "CMakeFiles/ecat_test.elf.dir/Core/Src/stm32f4xx_it.c.obj" "gcc" "CMakeFiles/ecat_test.elf.dir/Core/Src/stm32f4xx_it.c.obj.d"
+  "D:/fw/motor_ws/ecat/ecat_test/Core/Src/syscalls.c" "CMakeFiles/ecat_test.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/ecat_test.elf.dir/Core/Src/syscalls.c.obj.d"
+  "D:/fw/motor_ws/ecat/ecat_test/Core/Src/sysmem.c" "CMakeFiles/ecat_test.elf.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/ecat_test.elf.dir/Core/Src/sysmem.c.obj.d"
+  "D:/fw/motor_ws/ecat/ecat_test/Core/Src/system_stm32f4xx.c" "CMakeFiles/ecat_test.elf.dir/Core/Src/system_stm32f4xx.c.obj" "gcc" "CMakeFiles/ecat_test.elf.dir/Core/Src/system_stm32f4xx.c.obj.d"
+  "D:/fw/motor_ws/ecat/ecat_test/Core/Src/tim.c" "CMakeFiles/ecat_test.elf.dir/Core/Src/tim.c.obj" "gcc" "CMakeFiles/ecat_test.elf.dir/Core/Src/tim.c.obj.d"
+  "D:/fw/motor_ws/ecat/ecat_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "CMakeFiles/ecat_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj" "gcc" "CMakeFiles/ecat_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj.d"
+  "D:/fw/motor_ws/ecat/ecat_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "CMakeFiles/ecat_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj" "gcc" "CMakeFiles/ecat_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj.d"
+  "D:/fw/motor_ws/ecat/ecat_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "CMakeFiles/ecat_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj" "gcc" "CMakeFiles/ecat_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj.d"
+  "D:/fw/motor_ws/ecat/ecat_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "CMakeFiles/ecat_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj" "gcc" "CMakeFiles/ecat_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj.d"
+  "D:/fw/motor_ws/ecat/ecat_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c" "CMakeFiles/ecat_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj" "gcc" "CMakeFiles/ecat_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj.d"
+  "D:/fw/motor_ws/ecat/ecat_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "CMakeFiles/ecat_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj" "gcc" "CMakeFiles/ecat_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj.d"
+  "D:/fw/motor_ws/ecat/ecat_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "CMakeFiles/ecat_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/ecat_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj.d"
+  "D:/fw/motor_ws/ecat/ecat_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "CMakeFiles/ecat_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj" "gcc" "CMakeFiles/ecat_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj.d"
+  "D:/fw/motor_ws/ecat/ecat_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "CMakeFiles/ecat_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj" "gcc" "CMakeFiles/ecat_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj.d"
+  "D:/fw/motor_ws/ecat/ecat_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "CMakeFiles/ecat_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj" "gcc" "CMakeFiles/ecat_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj.d"
+  "D:/fw/motor_ws/ecat/ecat_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "CMakeFiles/ecat_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/ecat_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj.d"
+  "D:/fw/motor_ws/ecat/ecat_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "CMakeFiles/ecat_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj" "gcc" "CMakeFiles/ecat_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj.d"
+  "D:/fw/motor_ws/ecat/ecat_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "CMakeFiles/ecat_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/ecat_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj.d"
+  "D:/fw/motor_ws/ecat/ecat_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_spi.c" "CMakeFiles/ecat_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_spi.c.obj" "gcc" "CMakeFiles/ecat_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_spi.c.obj.d"
+  "D:/fw/motor_ws/ecat/ecat_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "CMakeFiles/ecat_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj" "gcc" "CMakeFiles/ecat_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj.d"
+  "D:/fw/motor_ws/ecat/ecat_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "CMakeFiles/ecat_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/ecat_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
